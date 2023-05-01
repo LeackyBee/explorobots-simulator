78466da2d09b04dcd2f922837860cda0e205c7a9
LeackyBee <alec.kb@outlook.com>
2023-05-01 16:52:44 +0100

    Woops

--- simulator/src/main/java/io/github/leackybee/main/Constants.java
+++ simulator/src/main/java/io/github/leackybee/main/Constants.java
@@ -5,4 +5,7 @@ public final class Constants {
     public static int MAP_WIDTH = 800;
     public static int MAP_HEIGHT = 600;
 
+    public static String MAP_DIRECTORY = "simulator/src/main/maps/";
+    public static String MAP = "smiley.png";
+
 }
