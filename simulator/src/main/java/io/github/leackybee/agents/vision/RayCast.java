d4ae43f93781cbe8dc82a493e79b11909d139dd2
LeackyBee <alec.kb@outlook.com>
2023-05-04 18:05:51 +0100

    Fixed RayCast

--- simulator/src/main/java/io/github/leackybee/agents/vision/RayCast.java
+++ simulator/src/main/java/io/github/leackybee/agents/vision/RayCast.java
@@ -16,14 +16,15 @@ public class RayCast extends VisionHandler{
     public void scan(OccupancyGrid occGrid, RealMap map, Point position, double heading) {
         // We send 2 rays for every pixel on the outer perimeter of the visual cone
         // One to the center of the pixel, and one to the edge.
-        double divisions = (4*Math.PI * (FoV/2*Math.PI)*2*visualRange);
+        int divisions = (int) (0.1*Math.PI * (FoV/2*Math.PI)*2*visualRange);
 
-        for(double h = heading - FoV/2; h <= heading + FoV/2; h += FoV/divisions){
+        for(double h = heading - FoV/2; h <= heading + FoV/2; h += FoV/(divisions-1)){
             double tx = Math.sin(h)*visualRange;
             double ty = -Math.cos(h)*visualRange;
 
-            List<Point> ray = occGrid.getLinePoints(position.x,position.y,Math.round(tx), Math.round(ty));
+            List<Point> ray = occGrid.getLinePoints((int) position.x, (int)position.y, (int) Math.round(tx), (int) Math.round(ty));
             for(Point point : ray){
+                occGrid.setFocus(point);
                 if(map.checkTile(point) == RealMap.TileState.Wall){
                     occGrid.setWall(point);
                     break;
@@ -31,6 +32,7 @@ public class RayCast extends VisionHandler{
                     occGrid.setFree(point);
                 }
             }
+            //System.out.println(occGrid);
 
         }
     }
