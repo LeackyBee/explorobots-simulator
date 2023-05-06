f0dc5b0d2132a895267ec688068f5eb3849e79d4
LeackyBee <alec.kb@outlook.com>
2023-05-06 12:55:21 +0100

    Re-implemented Main.main to be able to test without Vaadin web UI

---
+++ src/main/java/io/github/leackybee/simulator/Main.java
@@ -0,0 +1,9 @@
+package io.github.leackybee.simulator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimulationCycle sim = new SimulationCycle();
+        sim.start(1);
+    }
+}
